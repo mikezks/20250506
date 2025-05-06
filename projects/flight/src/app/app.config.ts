28e65be38ff9413094b582fcea42e7556d9d979c
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-06 13:44:32 +0200

    fix: interceptors from di

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
@@ -6,16 +6,24 @@ import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { APP_ROUTES } from './app.routes';
 import { provideRouterFeature } from './shared/logic-router-state';
+import { AuthInterceptor } from './shared/logic-communication/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES,
       withComponentInputBinding()
     ),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
     provideStore(),
     provideEffects(),
     provideRouterFeature(),
-    provideStoreDevtools()
+    provideStoreDevtools(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: AuthInterceptor
+    }
   ]
 };
