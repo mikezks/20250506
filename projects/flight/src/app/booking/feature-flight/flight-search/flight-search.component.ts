7b987589a1155596a921f4d24b24b6cc277fdf08
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-06 14:50:23 +0200

    fix: tidy-up

--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -1,9 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Injector, runInInjectionContext } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight, FlightFilter, injectTicketsFacade } from '../../logic-flight';
 import { FlightCardComponent, FlightFilterComponent } from '../../ui-flight';
-import { FlightService } from '../../logic-flight/data-access/flight.service';
 
 
 @Component({
@@ -18,7 +17,6 @@ import { FlightService } from '../../logic-flight/data-access/flight.service';
 })
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
-  private injector = inject(Injector);
 
   protected filter = {
     from: 'London',
@@ -30,19 +28,8 @@ export class FlightSearchComponent {
     5: true
   };
   protected flights$ = this.ticketsFacade.flights$;
-
-  constructor() {
-    // this.flightService.findById(5).subscribe(console.log);
-  }
   
-  protected search(filter: FlightFilter): void {
-    runInInjectionContext(
-      this.injector,
-      () => inject(FlightService).findById(4).subscribe(console.log)
-    );
-
-    this.injector.get(FlightService).findById(3).subscribe(console.log);
-    
+  protected search(filter: FlightFilter): void {    
     this.filter = filter;
 
     if (!this.filter.from || !this.filter.to) {
