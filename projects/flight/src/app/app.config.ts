c8c04cfa88b3cb97b8619aed210b1c777528825a
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-07 15:24:03 +0200

    demo: ssr, hydration, incremental hydration, event replay

--- projects/flight/src/app/app.config.ts
+++ projects/flight/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, inject, PLATFORM_ID, provideAppInitializer } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
@@ -8,6 +8,9 @@ import { APP_ROUTES } from './app.routes';
 import { provideRouterFeature } from './shared/logic-router-state';
 import { provideNavigationService } from './shared/logic-navigation';
 import { APP_NAVIGATION } from './app.navigation';
+import { provideClientHydration, withEventReplay, withIncrementalHydration } from '@angular/platform-browser';
+import { isPlatformServer } from '@angular/common';
+import { of, delay } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,6 +22,16 @@ export const appConfig: ApplicationConfig = {
     provideEffects(),
     provideRouterFeature(),
     provideStoreDevtools(),
-    provideNavigationService(APP_NAVIGATION)
+    provideNavigationService(APP_NAVIGATION),
+    provideClientHydration(
+      withEventReplay(),
+      withIncrementalHydration()
+    ),
+    provideAppInitializer((
+      id = inject(PLATFORM_ID)
+    ) => isPlatformServer(id)
+      ? of(true)
+      : of(true).pipe(delay(10_000))
+    )
   ]
 };
--- projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
+++ projects/flight/src/app/booking/feature-flight/flight-search/flight-search.component.ts
@@ -38,6 +38,8 @@ export class FlightSearchComponent {
       const route = this.route();
       untracked(() => this.logRoute(route));
     });
+
+    this.search(this.filter());
   }
 
   logRoute(route: string): void {
@@ -55,7 +57,7 @@ export class FlightSearchComponent {
   }
 
   protected delay(flight: Flight): void {
-    const oldFlight = flight;
+    const oldFlight = this.flights().find(item => item.id === flight.id) || flight;
     const oldDate = new Date(oldFlight.date);
 
     const newDate = new Date(oldDate.getTime() + 1000 * 60 * 5); // Add 5 min
