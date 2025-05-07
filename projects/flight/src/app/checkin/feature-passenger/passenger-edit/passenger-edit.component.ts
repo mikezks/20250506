7bb9c70cd404fde7e03310c17e9fcd6d95a853cf
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2025-05-07 11:35:05 +0200

    refactor: passenger edit rxjs interop

--- projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ projects/flight/src/app/checkin/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,23 +1,39 @@
 import { NgIf } from '@angular/common';
-import { Component, effect, inject, input, numberAttribute, untracked } from '@angular/core';
+import { Component, effect, inject, input, numberAttribute, signal, untracked } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { validatePassengerStatus } from '../../util-validation';
+import { initialPassenger } from '../../logic-passenger';
+import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
+import { switchMap } from 'rxjs';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-passenger-edit',
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLink
   ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
+  private passengerService = inject(PassengerService);
+
   id = input.required({ transform: numberAttribute });
+  id$ = toObservable(this.id);
+  passenger$ = this.id$.pipe(
+    switchMap(id => this.passengerService.findById(id))
+  );
+  passenger = toSignal(this.passenger$, {
+    // requireSync: true,
+    initialValue: initialPassenger
+  });
 
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
-    firstName: [''],
+    firstName: ['INIT'],
     name: [''],
     bonusMiles: [0],
     passengerStatus: ['', [
@@ -26,9 +42,9 @@ export class PassengerEditComponent {
   });
 
   constructor() {
-    effect(() => {
-      untracked(() => console.log(this.id()));
-    });
+    effect(() => this.editForm.patchValue(
+      this.passenger()
+    ));
   }
 
   protected save(): void {
